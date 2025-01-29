b39f071fe47948ce0f7b48e83fb4a800585a8921
krishna <mandalkrishna739@gmail.com>
2025-01-29 22:09:43 +0545

    Active class sidebar

--- resources/views/includes/auth/sidebar.blade.php
+++ resources/views/includes/auth/sidebar.blade.php
@@ -4,14 +4,14 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="{{ route('backend.dashboard') }}">
+        <a class="nav-link @if(Request::segment(2) != 'dashboard') collapsed @endif" href="{{ route('backend.dashboard') }}">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('backend.user.list') }}">
+        <a class="nav-link  @if(Request::segment(2) != 'user') collapsed @endif" href="{{ route('backend.user.list') }}">
           <i class="bi bi-person"></i>
           <span>Users</span>
         </a>
@@ -19,7 +19,7 @@
 
       <li class="nav-item">
         {{-- {{ route('backend.category.list') }} --}}
-        <a class="nav-link collapsed" href="">
+        <a class="nav-link  @if(Request::segment(2) != 'category') collapsed @endif" href="">
           <i class="bi bi-person"></i>
           <span>Category</span>
         </a>
@@ -27,7 +27,7 @@
 
       <li class="nav-item">
         {{-- {{ route('backend.blog.list') }} --}}
-        <a class="nav-link collapsed" href="">
+        <a class="nav-link @if(Request::segment(2) != 'blog') collapsed @endif" href="">
           <i class="bi bi-question-circle"></i>
           <span>Blog</span>
         </a>
@@ -35,7 +35,7 @@
 
       <li class="nav-item">
         {{-- {{ route('backend.help.list') }} --}}
-        <a class="nav-link collapsed" href="">
+        <a class="nav-link @if(Request::segment(2) != 'help') collapsed @endif" href="">
           <i class="bi bi-envelope"></i>
           <span>Help</span>
         </a>
@@ -43,11 +43,11 @@
 
       <li class="nav-item">
         {{-- {{ route('backend.inbox.list') }} --}}
-        <a class="nav-link collapsed" href="">
+        <a class="nav-link @if(Request::segment(2) != 'inbox') collapsed @endif" href="">
           <i class="bi bi-card-list"></i>
           <span>Inbox</span>
         </a>
-      </li><!-- End Register Page Nav -->
+      </li>
     </ul>
 
-  </aside><!-- End Sidebar-->
+  </aside>
